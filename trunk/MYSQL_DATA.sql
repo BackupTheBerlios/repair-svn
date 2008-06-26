3d1096fe11cbd41ce75dd94fe0e41bd176040eee
ibaun <ibaun@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-06-26 18:20:45 +0000

    Mysql databank structuur én de data die als basis kan dienen voor een applicatie.

---
+++ trunk/MYSQL_DATA.sql
@@ -0,0 +1,80 @@
+-- 
+-- Dumping data for table `categorie`
+-- 
+
+INSERT INTO `categorie` (`id`, `naamNL`, `naamEN`, `locatie`) VALUES 
+(1, 'Elektriciteit', 'Electricity', 'kot'),
+(2, 'Elektriciteit', 'Electricity', 'verdiep'),
+(3, 'Meubilair', 'Furniture', 'kot'),
+(4, 'Elektriciteit', 'Electricity', 'gemeenschappelijk'),
+(5, 'Loodgieterswerk', 'Plumbing', 'kot'),
+(6, 'Loodgieterswerk', 'Plumbing', 'verdiep'),
+(7, 'Allerlei', 'Extra', 'kot');
+
+-- 
+-- Dumping data for table `herstelformulier`
+-- 
+
+INSERT INTO `herstelformulier` (`id`, `datum`, `status`, `userId`, `kamer`, `homeId`, `opmerking`) VALUES 
+(1, '2008-06-26 20:14:29', 'ongezien', 2, '91.01.240.030', 1, 'Dit is mijn eerste herstelformuliertje ooit, cool hé!'),
+(2, '2008-06-25 20:14:58', 'gezien', 1, '91.01.230.012', 1, 'Dit herstelformulier is al gezien, maar nog niet uitgevoerd.'),
+(3, '2008-06-17 20:15:45', 'gedaan', 1, '91.01.230.012', 1, 'Dit herstelformulier is uitgevoerd, maar niet geëvalueerd.'),
+(4, '2008-06-03 20:16:03', 'afgesloten', 2, '91.01.240.030', 1, 'Dit herstelformulier is uitgevoerd en afgesloten door de student.');
+
+-- 
+-- Dumping data for table `home`
+-- 
+
+INSERT INTO `home` (`id`, `naam`, `adres`, `verdiepen`) VALUES 
+(1, 'Home Koning Boudewijn', 'Harelbekestraat 70', 14),
+(2, 'Home Koningin Astrid', 'Krijgslaan 250', 5),
+(3, 'Home Koningin Fabiola', 'Stalhof 4', 7),
+(4, 'Home August Vermeylen', 'Stalhof 6', 8),
+(5, 'Home Bertha De Vriese', 'De Pintelaan 260B', 4),
+(6, 'Home Corneel Heymans', 'Isabellakaai 120', 7);
+
+-- 
+-- Dumping data for table `personeel`
+-- 
+
+INSERT INTO `personeel` (`userId`) VALUES 
+(1),
+(2);
+
+-- 
+-- Dumping data for table `student`
+-- 
+
+INSERT INTO `student` (`userId`, `taal`, `homeId`, `kamer`, `telefoon`) VALUES 
+(1, 'nl', 1, '91.01.230.012', 14715),
+(2, 'nl', 1, '91.01.240.030', 14765);
+
+-- 
+-- Dumping data for table `user`
+-- 
+
+INSERT INTO `user` (`id`, `gebruikersnaam`, `laatsteOnline`, `email`) VALUES 
+(1, 'bmesuere', '2008-06-26 19:56:05', 'bart.mesuere@ugent.be'),
+(2, 'bevdeghi', '2008-06-26 19:56:23', 'bert.vandeghinste@ugent.be');
+
+-- 
+-- Dumping data for table `velden`
+-- 
+
+INSERT INTO `velden` (`id`, `naamNL`, `naamEN`, `categorieId`, `homeId`, `verwijderd`) VALUES 
+(1, 'Lamp boven lavabo', 'Lamp above the sink', 1, 1, 0),
+(2, 'Bed: poot afgebroken', 'Bed: leg is broken', 3, 1, 0),
+(3, 'Grote lichten', 'Striplighting', 1, 1, 0),
+(4, 'Lamp boven bed', 'Lamp above the bed', 1, 1, 0),
+(5, 'Schakelaar boven lavabo', 'Switch of lamp above the sink', 1, 1, 0),
+(6, 'Schakelaar grote lichten', 'Switch of striplighting', 1, 1, 0),
+(7, 'Schakelaar lamp boven bed', 'Switch of lamp above the bed', 1, 1, 0),
+(8, 'Stopcontacten', 'Sockets', 1, 1, 0),
+(9, 'Sifon lavabo lekt', 'Drain sink drips', 5, 1, 0),
+(10, 'Kraan lavabo lekt', 'Tap sink leaks', 5, 1, 0),
+(11, 'Lavabo verstopt', 'Sinkdrain is clogged', 5, 1, 0),
+(12, 'Radiator sluit niet af', 'Radiator can not be turned off', 5, 1, 0),
+(13, 'Radiator kraan lekt', 'Radiator tap leaks', 5, 1, 0),
+(14, 'Radiator geeft geen warmte', 'No heating from radiator', 5, 1, 0),
+(15, 'Skandiaflex', 'Persian blind', 7, 1, 0),
+(16, 'Scharnier kast', 'Wardrobe hinge', 3, 1, 0);
---
+++ trunk/MYSQL_STRUCTURE.sql
@@ -0,0 +1,107 @@
+-- 
+-- Table structure for table `categorie`
+-- 
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL auto_increment,
+  `naamNL` varchar(255) NOT NULL,
+  `naamEN` varchar(255) NOT NULL,
+  `locatie` enum('kot','verdiep','gemeenschappelijk') NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `herstelformulier`
+-- 
+
+DROP TABLE IF EXISTS `herstelformulier`;
+CREATE TABLE `herstelformulier` (
+  `id` int(11) NOT NULL auto_increment,
+  `datum` datetime NOT NULL,
+  `status` enum('ongezien','gezien','gedaan','afgesloten') NOT NULL,
+  `userId` int(11) NOT NULL,
+  `kamer` varchar(13) NOT NULL,
+  `homeId` int(11) NOT NULL,
+  `opmerking` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `home`
+-- 
+
+DROP TABLE IF EXISTS `home`;
+CREATE TABLE `home` (
+  `id` int(11) NOT NULL auto_increment,
+  `naam` varchar(255) NOT NULL,
+  `adres` varchar(255) NOT NULL,
+  `verdiepen` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `naam` (`naam`,`adres`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `personeel`
+-- 
+
+DROP TABLE IF EXISTS `personeel`;
+CREATE TABLE `personeel` (
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY  (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `student`
+-- 
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `userId` int(11) NOT NULL,
+  `taal` enum('nl','en') NOT NULL,
+  `homeId` int(11) NOT NULL,
+  `kamer` varchar(13) NOT NULL,
+  `telefoon` int(5) NOT NULL,
+  UNIQUE KEY `userId` (`userId`,`kamer`,`telefoon`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `user`
+-- 
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `gebruikersnaam` varchar(8) NOT NULL,
+  `laatsteOnline` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `gebruikersnaam` (`gebruikersnaam`,`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `velden`
+-- 
+
+DROP TABLE IF EXISTS `velden`;
+CREATE TABLE `velden` (
+  `id` int(11) NOT NULL auto_increment,
+  `naamNL` varchar(255) NOT NULL,
+  `naamEN` varchar(255) NOT NULL,
+  `categorieId` int(11) NOT NULL,
+  `homeId` int(11) NOT NULL,
+  `verwijderd` tinyint(1) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
