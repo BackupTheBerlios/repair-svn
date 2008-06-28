e355e445f845f6c42a457e9d32fcfe5d0eb719ee
bmesuere <bmesuere@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-06-28 17:43:04 +0000

    eerste versie van inloggen in Index.php

--- trunk/index.php
+++ trunk/index.php
@@ -1,5 +1,25 @@
-<a href="pages/help.php">klik</a><br/>
+<ul>
+	<li><a href="pages/help.php">klik</a></li>
+	<li><a href="https://webauth.ugent.be/?aid=rep&url=<? echo "http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];?>" >log in</a></li>
+</ul>
 <?
+
+// test voor inloggen
+$pubkey = openssl_get_publickey('file://ugent.pub') or die();
+if (isset($_GET['key'])) {
+    $ticket = $_GET['key'];
+    $ticket = strtr($ticket,'*-.','+/=');
+    $ticket = base64_decode($ticket);
+    if (openssl_public_decrypt($ticket,$data,$pubkey)) {
+        $_data = explode(":",$data);
+        list($user,$time,$aid) = $_data;
+        echo "user : $user </br>";
+		echo "time : ".strftime('%Y/%m/%dT %H:%M:%S', $time)." </br>";
+		echo "aid  : $aid</br>:";
+    }
+}
+
+//test van bert
 require_once("classes/Herstelformulier.class.php");
 require_once("classes/Veld.class.php");
 
