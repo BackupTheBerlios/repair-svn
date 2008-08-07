5d4ab0f57b635290705e0b79a8db4e4092d4807e
ibaun <ibaun@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-08-07 16:57:01 +0000

    - include vergeten

--- trunk/ajax/evaluatieMelding.php
+++ trunk/ajax/evaluatieMelding.php
@@ -1,6 +1,7 @@
 <?php
 require_once("../classes/Herstelformulier.class.php");
 require_once("../classes/Status.class.php");
+require_once("../classes/exceptions/BadParameterException.class.php");
 $formid = $_POST['formid'];
 if (!is_numeric($formid) || $formid < 1) throw new BadParameterException("Formid is ongeldig."); // TODO: gepaste exception
 $evaluatie = $_POST['evaluatie'];
