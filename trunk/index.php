6d801362de8e16e8656846bf9debb5eb0b2e1026
bmesuere <bmesuere@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-08-08 19:10:14 +0000

    foutje

--- trunk/index.php
+++ trunk/index.php
@@ -16,7 +16,6 @@
 		$t = "NL";
 	}
 	$taal = new Taal($t);
-	$taal =  new Taal($auth->getUser()->getTaal());
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
