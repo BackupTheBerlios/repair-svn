aa4491a1cbf94fded3e55dc9c2e2a5ed823d17a3
ibaun <ibaun@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-08-12 18:22:00 +0000

    buchtmail :(

--- trunk/cron.php
+++ trunk/cron.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'classes/Config.class.php';
 require_once 'Herstelformulier.class.php';
+require_once 'Status.class.php';
+require_once 'Mailer.class.php';
 
 // overloop alle herstelformulieren die voldoen aan evaluatiecriteria
 $list = Herstelformulier::getEvaluationList();
@@ -8,4 +10,20 @@ $list = Herstelformulier::getEvaluationList();
 foreach ($list as $formulier) {
 	echo $formulier->getId()."<br/>";
 }
+
+// doorzenden naar Homemanager als er nieuwe herstelformulieren zijn
+$list = Herstelformulier::getList(0, new Status("ongezien"));
+if (sizeof($list) == 0) {
+	echo sizeof($list)." ongeziene herstelformulieren.";
+}
+
+$mailer = new Mailer();
+$mailer->setFrom("bert.vandeghinste@gmail.com");
+$mailer->setSubject("cron test");
+$mailer->setPriority("high");
+$mailer->setText("testje!");
+$return = $mailer->send(array("bert.vandeghinste@gmail.com"));
+
+echo $return;        
+
 ?>
