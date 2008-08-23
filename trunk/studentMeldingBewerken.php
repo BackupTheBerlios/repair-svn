3f44696ad9ca6d7784e44157aa15f80f572111a9
Bert Vandeghinste <Bert Vandeghinste@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-08-23 18:04:31 +0000

    verwijderd-veld bug gefixed in studentMeldingBewerken

--- trunk/studentMeldingBewerken.php
+++ trunk/studentMeldingBewerken.php
@@ -87,23 +87,39 @@
 					<table>
 							<tr class="tabelheader"><td colspan="4"><? printf($taal->msg('herstelformulier_homenaam'),$currentHome->getKorteNaam()); ?></td></tr>
 							<?
-								$lijst = Veld::getHomeForm($currentHome);
-	
-								for($i=0; $i < sizeof($lijst);$i++){
-									$veld = $lijst[$i];
-									$nieuweCategorie = $veld->getCategorie();
-									$nieuweLocatie = $nieuweCategorie->getLocatie();
-									if (!isset($huidigeLocatie) || ($huidigeLocatie->getValue() != $nieuweLocatie->getValue())) {
-										$huidigeLocatie = $nieuweLocatie;
-										echo("<tr class='subheader' id='locatie_".$huidigeLocatie->getValue()."'><td colspan='4'>".$huidigeLocatie->getValue()."</td></tr>");
+									$huidigeLijst = Veld::getHomeForm($currentHome); // array<Veld>
+									$ingevuldeLijst = $formulier->getVeldenlijst(); // array<VeldID>
+									// vergelijking van huidigelijst met ingevuldeLijst. Alle velden van huidigeLijst moeten overblijven + velden die in ingevuldeLijst zitten zonder duplicates te maken
+									
+									// TODO: een treffelijk algoritme voor dit probleem
+									foreach($ingevuldeLijst as $veldid) {
+										$veld = new Veld($veldid);
+										$positie = 0; // positie om het Veld in te voegen in HuidigeLijst (adhv de Locatie en categorie)
+										if ($veld->getVerwijderd()) { // veld zal niet in huidigeLijst zitten
+											for ($i = 0; $i < sizeof($huidigeLijst); $i++) {
+												$huidigVeld = $huidigeLijst[$i];
+												if ($huidigVeld->getCategorie()->getLocatie() == $veld->getCategorie()->getLocatie() &&
+													$huidigVeld->getCategorie()->getNaamNL() == $veld->getCategorie()->getNaamNL())
+													$positie = $i;
+											}
+											array_splice($huidigeLijst, $positie, 0, array($veld));
+										}
 									}
-									if (!isset($huidigeCategorie) || ($huidigeCategorie->getNaamNL() != $nieuweCategorie->getNaamNL())) {
-										$huidigeCategorie = $nieuweCategorie;
-										echo("<tr class='subsubheader klik ".$huidigeLocatie->getValue()."' id='cat_".$huidigeCategorie->getId()."' onclick='showGroup(".$huidigeCategorie->getId().");'><td width='12px' id='collapse_".$huidigeCategorie->getId()."'>+</td><td colspan='3'>".$huidigeCategorie->getNaam()."</td></tr>");
-										echo("<tr class='legende ".$huidigeLocatie->getValue()." ".$huidigeCategorie->getId()."' style='display:none'><td></td><td>Defect</td><td>".$taal->msg('naam')."</td><td></td></tr>");
+									
+									foreach ($huidigeLijst as $veld) {
+										$nieuweCategorie = $veld->getCategorie();
+										$nieuweLocatie = $nieuweCategorie->getLocatie();
+										if (!isset($huidigeLocatie) || ($huidigeLocatie->getValue() != $nieuweLocatie->getValue())) {
+											$huidigeLocatie = $nieuweLocatie;
+											echo("<tr class='subheader' id='locatie_".$huidigeLocatie->getValue()."'><td colspan='4'>".$huidigeLocatie->getValue()."</td></tr>");
+										}
+										if (!isset($huidigeCategorie) || ($huidigeCategorie->getNaamNL() != $nieuweCategorie->getNaamNL())) {
+											$huidigeCategorie = $nieuweCategorie;
+											echo("<tr class='subsubheader klik ".$huidigeLocatie->getValue()."' id='cat_".$huidigeCategorie->getId()."' onclick='showGroup(".$huidigeCategorie->getId().");'><td width='12px' id='collapse_".$huidigeCategorie->getId()."'>+</td><td colspan='3'>".$huidigeCategorie->getNaam()."</td></tr>");
+											echo("<tr class='legende ".$huidigeLocatie->getValue()." ".$huidigeCategorie->getId()."' style='display:none'><td></td><td>Defect</td><td>".$taal->msg('naam')."</td><td></td></tr>");
+										}
+										echo("<tr class='klik ".$huidigeLocatie->getValue()." ".$huidigeCategorie->getId()." DONTPrint' id='item_".$veld->getId()."' onclick='checkItem(".$veld->getId().");' style='display:none'><td></td><td><input id='check_".$veld->getId()."' type='checkbox' name='".$veld->getId()."' onclick='checkItem(".$veld->getId().");'/></td><td>".$veld->getNaam()."<td></tr>");
 									}
-									echo("<tr class='klik ".$huidigeLocatie->getValue()." ".$huidigeCategorie->getId()." DONTPrint' id='item_".$veld->getId()."' onclick='checkItem(".$veld->getId().");' style='display:none'><td></td><td><input id='check_".$veld->getId()."' type='checkbox' name='".$veld->getId()."' onclick='checkItem(".$veld->getId().");'/></td><td>".$veld->getNaam()."<td></tr>");
-								}
 							?>
 					</table>
 					<div><label for="opmerking"><?=$taal->msg('opmerking') ?>:</label><div><textarea name="opmerking" id="opmerking" cols="50" rows="8"></textarea></div></div>
