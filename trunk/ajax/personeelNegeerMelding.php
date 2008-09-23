aa9e7981099bd4673f9434de1bb4e28c3d71645a
ibaun <ibaun@430dbbb0-9450-0410-b95b-df47ab5ecfa3>
2008-09-23 11:35:45 +0000

    2 kleine updates

--- trunk/ajax/personeelNegeerMelding.php
+++ trunk/ajax/personeelNegeerMelding.php
@@ -5,6 +5,7 @@ require_once 'BadParameterException.class.php';
 require_once 'Herstelformulier.class.php';
 require_once 'Status.class.php';
 require_once 'Auth.class.php';
+require_once 'Error.class.php';
 
 $auth = new Auth(false);
 if (!$auth->getUser()->isPersoneel())
@@ -14,6 +15,8 @@ $formid = $_POST['formid'];
 if (!is_numeric($formid) || $formid < 1)
 	throw new BadParameterException("Formid ".htmlspecialchars($formid)." is een ongeldige parameter.");
 	
+$error = new Error(0, "Herstelformulier genegeerd: ".$formid, "personeelNegeerMelding.php", "18", $auth->getUser()->getGebruikersnaam());
+
 $herstelformulier = new Herstelformulier($formid);
 $herstelformulier->setStatus(new Status("afgesloten"));
 $herstelformulier->save();
